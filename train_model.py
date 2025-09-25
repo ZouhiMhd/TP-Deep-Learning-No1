1d80b1526d732568bf818fe14092016cadd65784
ZouhiMhd <hasszouma@gmail.com>
2025-09-25 16:29:57 +0100

    First version of the model

--- train_model.py
+++ train_model.py
@@ -1,7 +1,9 @@
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
- 
+import mlflow
+import mlflow.tensorflow
+
 #Chargement du jeu de donn es MNIST
 (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
  
@@ -12,7 +14,20 @@ x_test = x_test.astype("float32") / 255.0
 #Redimensionnement des images pour les r seaux fully-connected
 x_train = x_train.reshape(60000, 784)
 x_test = x_test.reshape(10000, 784)
-      
+
+
+
+#Variables pour les param tres
+EPOCHS = 5
+BATCH_SIZE = 128
+DROPOUT_RATE = 0.2
+#Lancement de la session de suivi MLflow
+with mlflow.start_run():
+# Enregistrement des param tres
+	mlflow.log_param("epochs", EPOCHS)
+	mlflow.log_param("batch_size", BATCH_SIZE)
+	mlflow.log_param("dropout_rate", DROPOUT_RATE)
+
 #Construction du mod le
 model = keras.Sequential([
 	keras.layers.Dense(512, activation='relu', input_shape=(784,)),
@@ -20,7 +35,7 @@ model = keras.Sequential([
 	keras.layers.Dense(10, activation='softmax')
 ])
  
- #Compilation du mod le
+#Compilation du mod le
 model.compile(
 optimizer='adam',
 loss='sparse_categorical_crossentropy',
@@ -36,10 +51,16 @@ batch_size=128,
 validation_split=0.1
 )
  
- # valuation du mod le
+# valuation du mod le
 test_loss, test_acc = model.evaluate(x_test, y_test)
 print(f"Pr cision sur les donn es de test: {test_acc:.4f}")
  
- #Sauvegarde du mod le
+#Sauvegarde du mod le
 model.save("mnist_model.h5")
 print(" Mod le sauvegard sous mnist_model.h5")
+
+# Enregistrement des m triques
+mlflow.log_metric("test_accuracy", test_acc)
+
+# Enregistrement du mod le complet
+mlflow.keras.log_model(model, "mnist-model")
